[FIX] more normalization

braindecode/models/biot.py
@@ -364,7 +364,7 @@ class BIOT(EEGModuleMixin, nn.Module):
     activation: nn.Module, default=nn.ELU
         Activation function class to apply. Should be a PyTorch activation
         module class like ``nn.ReLU`` or ``nn.ELU``. Default is ``nn.ELU``.
-    return_feature: bool, optional
+    return_features: bool, optional
         Changing the output for the neural network. Default is single tensor
         when return_feature is True, return embedding space too.
         Default is False.
@@ -391,7 +391,7 @@ class BIOT(EEGModuleMixin, nn.Module):
         num_layers=4,
         sfreq=200,
         hop_length=100,
-        return_feature=False,
+        return_features=False,
         n_outputs=None,
         n_chans=None,
         chs_info=None,
@@ -413,7 +413,7 @@ class BIOT(EEGModuleMixin, nn.Module):
         self.hop_length = hop_length
         self.nhead = nhead
         self.num_layers = num_layers
-        self.return_feature = return_feature
+        self.return_features = return_features
         if (self.sfreq != 200) & (self.sfreq is not None):
             warn(
                 "This model has only been trained on a dataset with 200 Hz. "
@@ -471,7 +471,7 @@ class BIOT(EEGModuleMixin, nn.Module):
         emb = self.encoder(x)
         x = self.final_layer(emb)
 
-        if self.return_feature:
+        if self.return_features:
             return x, emb
         else:
             return x

braindecode/models/ctnet.py
@@ -20,6 +20,7 @@ from braindecode.models.base import EEGModuleMixin
 from braindecode.models.eegconformer import (
     _FeedForwardBlock,
     _MultiHeadAttention,
+    _FinalLayer,
 )
 
 
@@ -57,7 +58,7 @@ class CTNet(EEGModuleMixin, nn.Module):
     ----------
     activation : nn.Module, default=nn.GELU
         Activation function to use in the network.
-    heads : int, default=4
+    nhead : int, default=4
         Number of attention heads in the Transformer encoder.
     emb_size : int, default=40
         Embedding size (dimensionality) for the Transformer encoder.
@@ -79,7 +80,9 @@ class CTNet(EEGModuleMixin, nn.Module):
         Dropout probability for the positional encoding in the Transformer.
     drop_prob_final : float, default=0.5
         Dropout probability before the final classification layer.
-
+    return_features: bool
+        If True, the forward method returns the features before the
+        last classification layer. Defaults to False.
 
     Notes
     -----
@@ -120,6 +123,7 @@ class CTNet(EEGModuleMixin, nn.Module):
         depth_multiplier: int = 2,
         pool_size_1: int = 8,
         pool_size_2: int = 8,
+        return_features: bool = False,
     ):
         super().__init__(
             n_outputs=n_outputs,
@@ -187,8 +191,10 @@ class CTNet(EEGModuleMixin, nn.Module):
             nn.Dropout(p=self.drop_prob_final),
         )
 
-        self.final_layer = nn.Linear(
-            in_features=emb_size * sequence_length, out_features=self.n_outputs
+        self.final_layer = _FinalLayer(
+            hidden_channels=emb_size * sequence_length,
+            n_classes=self.n_outputs,
+            return_features=return_features,
         )
 
     def forward(self, x: Tensor) -> Tensor: